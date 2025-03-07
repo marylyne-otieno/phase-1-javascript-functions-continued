89615e28b06fa1875ea48d1fbab9733202c2cf4b
marylyne <otienomarylyne76@gmail.com>
2025-03-07 09:03:46 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun("swim"));
+console.log(saturdayFun("dance"));
+
+
+
+function mondayWork(activity = "go to the office") {
+return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "special") {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -33,13 +33,13 @@ describe("index.js", () => {
     it("function exists", function() {
       expect(wrapAdjective).to.exist
     })
-    
+
     it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
       let result = wrapAdjective('*')
       let emphatic = result("a hard worker")
       expect(emphatic).to.equal("You are *a hard worker*!")
     });
-
+"
     it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
       let result = wrapAdjective("||")
       let emphatic = result("a dedicated programmer")
